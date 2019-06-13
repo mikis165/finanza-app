4fd5ca17e6a7f9c1a059413d304b64c8a681c84e
michele saladino <michelelewagon@gmail.com>
2019-06-13 17:16:04 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { IntroGuard } from './guards/intro.guard';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { 
+    path: 'menu', 
+    loadChildren: './pages/menu/menu.module#MenuPageModule',
+    canActivate: [ IntroGuard ]
+    
+  },
+  { path: 'intro', loadChildren: './pages/intro/intro.module#IntroPageModule' },
+  { 
+    path: '', 
+    redirectTo: 'menu/tracker', 
+    pathMatch: 'full' 
+  },
+  { path: 'filter-popover', loadChildren: './pages/filter-popover/filter-popover.module#FilterPopoverPageModule' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage';
+import { CashFlowModalPageModule } from './pages/cash-flow-modal/cash-flow-modal.module';
+import { FilterPopoverPageModule } from './pages/filter-popover/filter-popover.module';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  IonicStorageModule.forRoot(),
+  CashFlowModalPageModule,
+  FilterPopoverPageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
